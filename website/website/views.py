0403eb960f0da421495c02d162db731c72d1c4f6
Incentive <aminulpalash506@gmail.com>
2020-10-18 21:57:01 +0600

    movie api details added

--- website/website/views.py
+++ website/website/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 from django.views.generic.base import TemplateView
 import pandas as pd 
+import requests
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -12,6 +13,44 @@ cv = CountVectorizer()
 count_matrix = cv.fit_transform(movies_df['genres'])
 similarity = cosine_similarity(count_matrix)
 
+
+api_url = 'https://api.themoviedb.org/3/search/movie?api_key=41f7cc5e1134d127a09413c900d5f199'
+
+def get_cast(movie_id):
+    
+    p = 'https://api.themoviedb.org/3/movie/'+movie_id+'/credits?api_key=41f7cc5e1134d127a09413c900d5f199'
+    r = requests.get(url = p) 
+    con = r.json() 
+    cast = []
+   
+    for i in con['cast']:
+        # s = 'image.tmdb.org/t/p/w138_and_h175_face' + 
+        # print(s,type(s))
+        p = {
+            #
+            'character':i['character'],
+            'name':i['name'],
+            'profile_path': i['profile_path'],
+        }
+        cast.append(p)
+    return cast
+    
+
+def get_trailer(movie_id):
+    p = 'https://api.themoviedb.org/3/movie/'+movie_id+'/videos?api_key=41f7cc5e1134d127a09413c900d5f199'
+    r = requests.get(url = p) 
+    con = r.json() 
+    return 'https://www.youtube.com/watch?v=' + con['results'][0]['key']
+
+def get_genres(movie_id):
+    
+    url = 'https://api.themoviedb.org/3/movie/'+movie_id+'?api_key=41f7cc5e1134d127a09413c900d5f199'
+    r = requests.get(url = url) 
+    con = r.json() 
+    p = ''
+    for i in con['genres']:
+        p += i['name']+" "
+    return p, str(con['runtime']//60) +'h '+ str(con['runtime']%60 )+'m'
 
 
 class index(TemplateView):
@@ -25,13 +64,32 @@ class index(TemplateView):
 
     def post(self,request):
         self.movie_name = request.POST['movie']
-
-        if self.movie_list != '':
-            try:
-                self. movie_list = rcmd(self.movie_name)
-            except:
-                pass
-        return render(request, 'home.html',{'movie_list':self.movie_list})
+        
+        title = self.movie_name
+        PARAMS = {'query':title} 
+        # sending get request and saving the response as response object 
+        r = requests.get(url = api_url, params = PARAMS) 
+        config = r.json() 
+        movie_id = str(config['results'][0]['id'])
+        genres,runtime = get_genres(movie_id)
+        trailer = get_trailer(movie_id)
+        cast = get_cast(movie_id)
+        config = {
+            'title':config['results'][0]['title'],
+            'poster_path':config['results'][0]['poster_path'],
+            'overview':config['results'][0]['overview'],
+            'release_date':config['results'][0]['release_date'].split('-')[0],
+            'genres':genres,
+            'runtime':runtime,
+            'trailer':trailer,
+            'cast':cast,
+        }
+        # if self.movie_list != '':
+        #     try:
+        #         self. movie_list = rcmd(self.movie_name)
+        #     except:
+        #         pass
+        return render(request, 'home.html',{'config':config})
 
 
 
